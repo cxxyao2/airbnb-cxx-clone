db7130e65a59903fdbc69281cf0e6387a140ce2f
jennifer <cxxyao2@gmail.com>
2023-08-09 00:26:44 -0400

    airbnb clone init

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface EmptyStateProps {
+  title: string
+  subtitle: string
+}
+
+const EmptyState = ({title,subtitle}:EmptyStateProps) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default EmptyState
